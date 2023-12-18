e00283c02d46180f563c9e6cc80144e2e595acde
johnnyyork13 <johnnyyork13@hotmail.com>
2023-12-18 08:11:48 -0500

    init vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/reset.css';
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
